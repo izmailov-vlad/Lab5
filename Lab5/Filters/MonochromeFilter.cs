ee640e6eab6d0bec4dc9a9f531b23305521d1602
Vladislav <izmaylov.vlad2001@gmai.com>
2022-12-18 19:18:18 +0300

    Add connected area searcher

--- Lab5/Filters/MonochromeFilter.cs
+++ Lab5/Filters/MonochromeFilter.cs
@@ -4,52 +4,27 @@ using System.Collections.Generic;
 
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab5.Filters
 {
     internal class MonochromeFilter : IFilter
     {
-        public byte[][] Execute(Picture image)
+        public byte[][] Execute(byte[][] pixels)
         {
-            byte[][] pixels = image.Pixels;
-            for(int i = 0; i < image.Height; i++)
-            {
-                for(int j = 0; j < image.Width; j++)
-                {
-                    byte pixel = image.GetPixel(i, j).ToArgb();
+            int rows = pixels.GetUpperBound(0) + 1;    // количество строк
+            int columns = pixels.Length / rows; // кол-во столбцов
 
-                    float R = (float)((pixel & 0x00FF0000) >> 16); // красный
-                    float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
-                    float B = (float)(pixel & 0x000000FF); // синий
-                                                           // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
-                    R = G = B = (R + G + B) / 3.0f;
-                    // собираем новый пиксель по частям (по каналам)
-                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
-                    // добавляем его в Bitmap нового изображения
-                    image.bitmap.SetPixel(i, j, Color.FromArgb((int)newPixel));
+            byte[][] _pixels = pixels;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    _pixels[i][j] = _pixels[i][j] <= 50 ? _pixels[i][j] = 0 : _pixels[i][j] = 1;
                 }
             }
-          
 
-            for (int j = 0; j < pixels.Length; j++)
-            {
-                for (int i = 0; i < pixels[i].Length; i++)
-                {
-                    // получаем (i, j) пиксель
-                    UInt32 pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
-                    // получаем компоненты цветов пикселя
-                    float R = (float)((pixel & 0x00FF0000) >> 16); // красный
-                    float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
-                    float B = (float)(pixel & 0x000000FF); // синий
-                                                           // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
-                    R = G = B = (R + G + B) / 3.0f;
-                    // собираем новый пиксель по частям (по каналам)
-                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
-                    // добавляем его в Bitmap нового изображения
-                    output.SetPixel(i, j, Color.FromArgb((int)newPixel));
-                }
-            }
+            return _pixels;
         }
     }
 }
---
+++ Lab5/picture_utils/ConnectedAreaSearcher.cs
@@ -0,0 +1,58 @@
+﻿using Lab5.Models;
+using System.Collections.Generic;
+
+namespace Lab5.picture_utils
+{
+    internal class ConnectedAreaSearcher
+    {
+        public byte[][] Find(byte[][] pixelsLB)
+        {
+            int label = 0;
+            FindComponents(pixelsLB, label);
+            return pixelsLB;
+        }
+
+        private void FindComponents(byte[][] pixelsLB, int label)
+        {
+            int rows = pixelsLB.GetUpperBound(0) + 1;    // количество строк
+            int columns = pixelsLB.Length / rows; // кол-во столбцов
+            for (int L = 0; L < rows; L++)
+            {
+                for(int P = 0; P < columns; P++)
+                {
+                    if (pixelsLB[L][P] == 0) 
+                    {
+                        label++;
+                        Search(pixelsLB, label, L, P);
+                    }
+                }
+            }
+        }
+
+        private void Search(byte[][] pixelsLB, int label, int L, int P) 
+        {
+            pixelsLB[L][P] = (byte)label;
+            HashSet<Pair<int, int>> neighbors = Neighbors(L, P);
+
+            foreach(Pair<int, int> neighbor in neighbors)
+            {
+                if (pixelsLB[neighbor.First][neighbor.Second] == 0)
+                {
+                    Search(pixelsLB, label, neighbor.First, neighbor.Second);
+                }
+            }
+        }
+
+        private HashSet<Pair<int, int>> Neighbors(int L, int P)
+        {
+            HashSet<Pair<int, int>> neighbors = new HashSet<Pair<int, int>>
+            {
+                new Pair<int, int>(L + 1, P),
+                new Pair<int, int>(L - 1, P),
+                new Pair<int, int>(L, P + 1),
+                new Pair<int, int>(L, P - 1)
+            };
+            return neighbors;
+        }
+    }
+}
