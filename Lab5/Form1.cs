54e9bb4f5dfb015e3d7f4b9e455b36f6d84199ab
sergey_kachan <sergej-cachan@yandex.ru>
2022-12-23 21:14:02 +0300

    ser

--- Lab5/Form1.cs
+++ Lab5/Form1.cs
@@ -21,20 +21,10 @@ namespace Lab5
 
             Hemming hemmingDistance = new Hemming();
 
-<<<<<<< HEAD
 
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(mass2[i, j] + " ");
-                }
-                Console.Write("\n");
-            }
-=======
             Console.WriteLine(hemmingDistance.Compare("123456", "888888uuuu"));
             Console.WriteLine("\n");
->>>>>>> 42783ddabad339408962e6ac89d5704815887d05
+
             
         }
     }
